ec32adca22d08e90e862ad1c282532b8ef42f6b6
geemus <geemus@gmail.com>
2011-12-22 15:42:59 -0600

    whitespace fixes in api to align hash rockets

--- lib/heroku/api/apps.rb
+++ lib/heroku/api/apps.rb
@@ -4,57 +4,57 @@ module Heroku
     # DELETE /apps/:app
     def delete_app(app)
       request(
-        :expects => 200,
-        :method => :delete,
-        :path => "/apps/#{app}"
+        :expects  => 200,
+        :method   => :delete,
+        :path     => "/apps/#{app}"
       )
     end
 
     # GET /apps
     def get_apps
       request(
-        :expects => 200,
-        :method => :get,
-        :path => "/apps"
+        :expects  => 200,
+        :method   => :get,
+        :path     => "/apps"
       )
     end
 
     # GET /apps/:app
     def get_app(app)
       request(
-        :expects => 200,
-        :method => :get,
-        :path => "/apps/#{app}"
+        :expects  => 200,
+        :method   => :get,
+        :path     => "/apps/#{app}"
       )
     end
 
     # POST /apps
     def post_app(params={})
       request(
-        :expects => 202,
-        :method => :post,
-        :path => '/apps',
-        :query => app_params(params)
+        :expects  => 202,
+        :method   => :post,
+        :path     => '/apps',
+        :query    => app_params(params)
       )
     end
 
     # POST /apps/:app/server/maintenance
     def post_app_server_maintenance(app, new_server_maintenance)
       request(
-        :expects => 200,
-        :method => :post,
-        :path => "/apps/#{app}/server/maintenance",
-        :query => {'maintenance_mode' => maintenance_mode}
+        :expects  => 200,
+        :method   => :post,
+        :path     => "/apps/#{app}/server/maintenance",
+        :query    => {'maintenance_mode' => maintenance_mode}
       )
     end
 
     # PUT /apps/:app
     def put_app(app, params)
       request(
-        :expects => 200,
-        :method => :put,
-        :path => "/apps/#{app}",
-        :query => app_params(params)
+        :expects  => 200,
+        :method   => :put,
+        :path     => "/apps/#{app}",
+        :query    => app_params(params)
       )
     end
 
--- lib/heroku/api/config_vars.rb
+++ lib/heroku/api/config_vars.rb
@@ -4,28 +4,28 @@ module Heroku
     # DELETE /apps/:app/config_vars/:key
     def delete_config_var(app, key)
       request(
-        :expects => 200,
-        :method => :delete,
-        :path => "/apps/#{app}/config_vars/#{key}"
+        :expects  => 200,
+        :method   => :delete,
+        :path     => "/apps/#{app}/config_vars/#{key}"
       )
     end
 
     # GET /apps/:app/config_vars
     def get_config_vars(app)
       request(
-        :expects => 200,
-        :method => :get,
-        :path => "/apps/#{app}/config_vars"
+        :expects  => 200,
+        :method   => :get,
+        :path     => "/apps/#{app}/config_vars"
       )
     end
 
     # PUT /apps/:app/config_vars
     def put_config_vars(app, vars)
       request(
-        :expects => 200,
-        :method => :put,
-        :path => "/apps/#{app}/config_vars",
-        :query => {'body' => Heroku::OkJson.encode(vars)}
+        :expects  => 200,
+        :method   => :put,
+        :path     => "/apps/#{app}/config_vars",
+        :query    => {'body' => Heroku::OkJson.encode(vars)}
       )
     end
 
