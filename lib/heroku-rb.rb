fba321ed3eeea7c2bbf0bc459d757598003e012d
geemus <geemus@gmail.com>
2012-01-18 13:49:10 -0600

    expand path before unshifting it

--- lib/heroku-rb.rb
+++ lib/heroku-rb.rb
@@ -1,4 +1,4 @@
-$:.unshift(File.dirname(__FILE__)) unless
+$:.unshift(File.expand_path(File.dirname(__FILE__))) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'base64'
