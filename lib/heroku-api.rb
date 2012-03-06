170aefa1a806b876473aebd878ee119612f66ad3
geemus <geemus@gmail.com>
2012-03-06 11:14:35 -0800

    fix to use relative load paths

--- lib/heroku-api.rb
+++ lib/heroku-api.rb
@@ -1,1 +1,1 @@
-require "heroku/api"
+require(File.join(File.dirname(__FILE__), "heroku", "api"))
--- lib/heroku/api.rb
+++ lib/heroku/api.rb
@@ -3,6 +3,11 @@ require "cgi"
 require "excon"
 require "securerandom"
 
+__LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+unless $LOAD_PATH.include?(__LIB_DIR__)
+  $LOAD_PATH.unshift(__LIB_DIR__)
+end
+
 require "heroku/api/vendor/okjson"
 
 require "heroku/api/errors"
