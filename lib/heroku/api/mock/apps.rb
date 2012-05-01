38eabc79e315c2cc0db9b4865ca413d19b137ac2
geemus <geemus@gmail.com>
2012-05-01 10:38:42 -0500

    fix for web_url typo in rename

--- lib/heroku/api/mock/apps.rb
+++ lib/heroku/api/mock/apps.rb
@@ -147,7 +147,7 @@ module Heroku
             name = request_params[:query]['app[name]']
             app_data['git_url'] = "git@heroku.com:#{name}.git"
             app_data['name'] = name
-            app_data['web_url'] = "http://#{app}.herokuapp.com/"
+            app_data['web_url'] = "http://#{name}.herokuapp.com/"
           end
           if request_params[:query].has_key?('app[transfer_owner]')
             email = request_params[:query]['app[transfer_owner]']
